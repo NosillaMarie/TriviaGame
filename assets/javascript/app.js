e5e207942e2b4665ccafee0634c7cdef2aad3ca6
Allison Marie <aliederman@icloud.com>
2017-07-09 17:50:46 -0400

    scoring working properly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,81 +4,114 @@ var questions = [
         question: "What is the largest fish in the ocean?",
         number: 1,
         choices: ["Blue Whale", "Blue Marlin", "Great White Shark", "Whale Shark"],
-        correctAnswer: "Whale Shark"
+        correctAnswer: 3
     },
 
     {
         question: "What is the fastest fish in the ocean?",
         number: 2,
         choices: ["Sailfish", "Tuna", "Wahoo", "Mahi-Mahi"],
-        correctAnswer: "Sailfish"
+        correctAnswer: 0
     },
     {
         question: "What has the largest eyeball in the ocean?",
         number: 3,
         choices: ["Humpback Whale", "Giant Squid", "Great White Shark", "Bluefin Tuna"],
-        correctAnswer: "Giant Squid"
+        correctAnswer: 1
     },
     {
         question: "What is the most poisonous fish?",
         number: 4,
         choices: ["Lionfish", "Stonefish", "Pufferfish", "Bluespotted Stingray"],
-        correctAnswer: "Pufferfish"
+        correctAnswer: 2
     },
     {
         question: "Herman Melville, who wrote the book Moby Dick. What kind of whale was Moby Dick?",
         number: 5,
         choices: ["Killer Whale", "Sperm Whale", "Beluga Whale", "Great White Shark"],
-        correctAnswer: "Sperm Whale"
+        correctAnswer: 1
     },
 
     {
         question: "How many oceans are there in the world?",
         number: 6,
         choices: [2, 4, 5, 6],
-        correctAnswer: 5
+        correctAnswer: 2
     },
     {
         question: "How many seas are there in the world?",
         number: 7,
         choices: [5, 6, 7, 8],
-        correctAnswer: 7
+        correctAnswer: 2
     },
     {
         question: "What are the groups fish travel in called?",
         number: 8,
         choices: ["Flock", "Herd", "School", "Pack"],
-        correctAnswer: "School"
+        correctAnswer: 2
     }
 ];
 
 var currentQuestion = 0;
-var correctAnswer = 0;
+var correctAnswers = 0;
 var gameOver = false;
 
 $(document).ready(function () {
+    $(".trivia").hide();
+    $("#progressBar").hide();
 
-    //here we will display the first question
+    $("button").click(function () {
+        $(".trivia").show();
+        $(".nextButton").show();
+        $("button").hide();
+        $("#progressBar").show();
+
+        function progress(timeleft, timetotal, $element) {
+            var progressBarWidth = timeleft * $element.width() / timetotal;
+            $element.find('div').animate({
+                width: progressBarWidth
+            }, 500).html(timeleft);
+
+            if (timeleft > 0) {
+                setTimeout(function () {
+                        progress(timeleft - 1, timetotal, $element);
+                    },
+                    1000);
+
+                if (progress == 0) {
+                    console.log("We'll figure this out!");
+                    debugger;
+                    resetTrivia();
+                }
+            }
+        };
+        progress(5, 5, $('#progressBar'));
+    })
+
+
+    //here is the first question
     displayFirstQuestion();
+
     $(this).find(".errorMessage").hide();
     //display next question on click.
     $(this).find(".nextButton").on("click", function () {
 
-                   var value;
+
         if (!gameOver) {
-            
-            value =
-                $("input[type='radio']:checked").val();
-//if next question button is selected without answer choice
-            if (value == undefined) {
+
+            value = $("input[type=radio][name=optradio]:checked").val();
+            //if next question button is selected without answer choice idsplay error
+            if (value === undefined) {
                 $(document).find(".errorMessage").text("Please select an answer!");
 
                 $(document).find(".errorMessage").show();
             } else {
                 $(document).find(".errorMessage").hide();
-//if choice selected == 
+                //if choice selected == 
                 if (value == questions[currentQuestion].correctAnswer) {
-                    correctAnswer++;
+                console.log(value);
+
+                    correctAnswers++;
                 }
                 currentQuestion++;
 
@@ -87,7 +120,7 @@ $(document).ready(function () {
                 } else {
                     displayScore();
                     //when game is over ask if play again
-                    $(document).find(".nextButton").text("Play Again?");
+                    $(".nextButton").text("Play Again?");
 
                     gameOver = true;
                 }
@@ -95,7 +128,7 @@ $(document).ready(function () {
         } else {
             gameOver = false;
 
-            $(document).find(".nextButton").text("Next Question");
+            $(".nextButton").text("Next Question");
 
             resetTrivia();
             displayFirstQuestion();
@@ -104,6 +137,8 @@ $(document).ready(function () {
     });
 });
 
+
+
 //to display current question asked and answer choices.
 function displayFirstQuestion() {
 
@@ -111,7 +146,7 @@ function displayFirstQuestion() {
     var questionClass = $(document).find(".trivia > .question");
     var choiceList = $(document).find(".trivia > .choiceList");
     var numChoices = questions[currentQuestion].choices.length;
-
+console.log(question);
     //    console.log(numChoices);
 
     $(questionClass).text(question);
@@ -123,18 +158,19 @@ function displayFirstQuestion() {
     for (i = 0; i < numChoices; i++) {
         choice =
             questions[currentQuestion].choices[i];
-        $('<li><input type="radio" value=" + i + " name="optradio"/>' + choice + '</li>').appendTo(choiceList);
+        $('<li><input type="radio" value=' + i + ' name="optradio"/>' + choice + '</li>').appendTo(choiceList);
+//                console.log(question.choices[i]);
     }
 }
 
 function resetTrivia() {
     currentQuestion = 0;
-    correctAnswer = 0;
+    correctAnswers = 0;
     hideScore();
 }
 
 function displayScore() {
-    $(document).find(".trivia > .result").text("Your Score: " + correctAnswer + " out of: " + questions.length);
+    $(document).find(".trivia > .result").text("Your Score: " + correctAnswers + " out of: " + questions.length);
     $(document).find(".trivia > .result").show();
 }
 
